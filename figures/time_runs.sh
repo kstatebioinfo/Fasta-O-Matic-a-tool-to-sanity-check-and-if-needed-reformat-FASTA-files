260847f23fac2577a1d860bb10af98bc1536460a
kstatebioinfo <kstate.bioinformatics@gmail.com>
2015-08-15 21:08:11 -0400

    Updated test script to include unique steps

--- figures/time_runs.sh
+++ figures/time_runs.sh
@@ -11,10 +11,15 @@ echo "New line only Fasta-O-Matic execution:"
 echo "Header whitespace only Fasta-O-Matic execution:"
 /usr/bin/time -v python ~/read-cleaning-format-conversion/KSU_bioinfo_lab/fasta-o-matic/fasta_o_matic.py -f ~/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna -o ~/test_fasta_o_matic/out -c -s header_whitespace -q
 
+echo "Unique only Fasta-O-Matic execution:"
+/usr/bin/time -v python ~/read-cleaning-format-conversion/KSU_bioinfo_lab/fasta-o-matic/fasta_o_matic.py -f ~/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna -o ~/test_fasta_o_matic/out -c -s unique -q
+
 echo "All steps Fasta-O-Matic execution:"
-/usr/bin/time -v python ~/read-cleaning-format-conversion/KSU_bioinfo_lab/fasta-o-matic/fasta_o_matic.py -f ~/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna -o ~/test_fasta_o_matic/out1 -c -q
+/usr/bin/time -v python ~/read-cleaning-format-conversion/KSU_bioinfo_lab/fasta-o-matic/fasta_o_matic.py -f ~/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna -o ~/test_fasta_o_matic/out1 -c  -s unique wrap new_line header_whitespace -q
+
 echo "Wrap only setk execution:"
 /usr/bin/time -v /home/bionano/seqtk/seqtk seq -l 60 /home/bionano/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna > /home/bionano/test_fasta_o_matic/out2/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE_seqtk_wrap.fna
+
 echo "Wrap only seqret execution:"
 /usr/bin/time -v /home/bionano/EMBOSS-6.5.7/emboss/seqret -sequence ~/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna -outseq /home/bionano/test_fasta_o_matic/out3/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE_seqret.fna
  
